1dc56e76ecc0d52903a41d5d15d8c7d9a74310ca
Hernán Erasmo <dherasmo@gmail.com>
2022-04-22 11:01:41 -0300

    fix(iplkp): change query group to not exclusive

--- iplkp/__init__.py
+++ iplkp/__init__.py
@@ -9,6 +9,10 @@ def main():
     main_group.add_argument("-i", "--ip-address", dest="ip_addr", metavar="IP_ADDR", help="Fetch information for a single given IP address")
     main_group.add_argument("-b", "--bulk", dest="filename", metavar="INPUT_FILE", help="Read IP addresses in bulk from INPUT_FILE")
 
+    query_group = parser.add_argument_group(title="Available queries")
+    query_group.add_argument("-g", "--geo-ip", dest="just_geo", help="Only query Geo IP information for the given address or list of addresses", action="store_true")
+    query_group.add_argument("-r", "--rdap", dest="just_rdap", help="Only query RDAP information for the given address or list of addresses", action="store_true")
+
     parser.add_argument("-o", "--output", dest="save_output", metavar="OUTPUT_FILE", help="Write output to a given OUTPUT_FILE")
     parser.add_argument("-f", "--force", dest="overwrite", help="Overwrite contents of OUTPUT_FILE if it exists", action="store_true")
 
