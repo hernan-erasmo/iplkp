472d4e35ab4ed82a11d78410d7e0e9516adfd135
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 19:02:07 -0300

    lint(iplkp): fix ALL pylint warnings

--- iplkp/__init__.py
+++ iplkp/__init__.py
@@ -1,13 +1,22 @@
+"""
+iplkp main module. iplkp is a command line application which
+helps looking up one or multiple IPs RDAP or Geo IP information.
+"""
+
 import json
 import sys
 from iplkp.lookup import lookup
-import iplkp.utils as utils
+from iplkp import utils
 
 def main():
+    """
+    iplkp entry point. Parse arguments, lookup addresses
+    and return or write results to file.
+    """
     try:
         args = utils.parse_args(sys.argv)
-    except utils.IplkpArgumentException as e:
-        print(f"{e}")
+    except utils.IplkpArgumentException as iplkp_arg_exception:
+        print(f"{iplkp_arg_exception}")
         sys.exit(1)
     else:
         valid_addrs = utils.parse_address_args(args)
@@ -15,10 +24,13 @@ def main():
     if not valid_addrs:
         sys.exit(1)
 
-    results = lookup(valid_addrs, just_rdap=args.just_rdap, just_geo=args.just_geo, use_cache=args.use_cache)
+    results = lookup(valid_addrs,
+                    just_rdap=args.just_rdap,
+                    just_geo=args.just_geo,
+                    use_cache=args.use_cache)
 
     if args.save_output:
-        with open(args.save_output, "w") as output:
+        with open(args.save_output, "w", encoding="utf-8") as output:
             json.dump(results, output)
     else:
         print(results)
