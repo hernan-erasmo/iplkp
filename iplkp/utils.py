881fe7a6a912a28820c207cd8159cab5a5282973
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 19:08:11 -0300

    lint(utils): fixed line length warnings

--- iplkp/utils.py
+++ iplkp/utils.py
@@ -22,16 +22,48 @@ def show_remaining_tasks():
 def parse_args(supplied_args):
     parser = argparse.ArgumentParser(description=IPLKP_DESC)
     main_group = parser.add_mutually_exclusive_group(required=True)
-    main_group.add_argument("-i", "--ip-address", dest="ip_addr", metavar="IP_ADDR", help="Fetch information for a single given IP address")
-    main_group.add_argument("-b", "--bulk", dest="filename", metavar="INPUT_FILE", help="Read IP addresses in bulk from INPUT_FILE")
+    main_group.add_argument("-i",
+                            "--ip-address",
+                            dest="ip_addr",
+                            metavar="IP_ADDR",
+                            help="Fetch information for a single given IP address")
+    main_group.add_argument("-b",
+                            "--bulk",
+                            dest="filename",
+                            metavar="INPUT_FILE",
+                            help="Read IP addresses in bulk from INPUT_FILE")
 
     query_group = parser.add_argument_group(title="Available queries")
-    query_group.add_argument("-g", "--geo-ip", dest="just_geo", help="Only query Geo IP information for the given address or list of addresses", action="store_true")
-    query_group.add_argument("-r", "--rdap", dest="just_rdap", help="Only query RDAP information for the given address or list of addresses", action="store_true")
+    query_group.add_argument("-g",
+                            "--geo-ip",
+                            dest="just_geo",
+                            help="Only query Geo IP information for the given " + \
+                                "address or list of addresses",
+                            action="store_true")
+    query_group.add_argument("-r",
+                            "--rdap",
+                            dest="just_rdap",
+                            help="Only query RDAP information for the given " + \
+                                "address or list of addresses",
+                            action="store_true")
 
-    parser.add_argument("-o", "--output", dest="save_output", metavar="OUTPUT_FILE", help="Write output to a given OUTPUT_FILE")
-    parser.add_argument("-f", "--force", dest="overwrite", help="Overwrite contents of OUTPUT_FILE if it exists", action="store_true")
-    parser.add_argument("-c", "--no-cache", dest="use_cache", help="Do not use cache to fetch results. Do not update it after getting IP information", default=True, action="store_false")
+    parser.add_argument("-o",
+                        "--output",
+                        dest="save_output",
+                        metavar="OUTPUT_FILE",
+                        help="Write output to a given OUTPUT_FILE")
+    parser.add_argument("-f",
+                        "--force",
+                        dest="overwrite",
+                        help="Overwrite contents of OUTPUT_FILE if it exists",
+                        action="store_true")
+    parser.add_argument("-c",
+                        "--no-cache",
+                        dest="use_cache",
+                        help="Do not use cache to fetch results. Do not update " + \
+                            "it after getting IP information",
+                        default=True,
+                        action="store_false")
 
     if len(supplied_args) == 1:
         parser.print_help()
@@ -40,7 +72,8 @@ def parse_args(supplied_args):
     args = parser.parse_args()
     output_file_exists = args.save_output and os.path.isfile(args.save_output)
     if output_file_exists and not args.overwrite:
-        raise IplkpArgumentException("Output file already exists. If you want to overwrite, you must pass the -f, --force flag.")
+        raise IplkpArgumentException("Output file already exists. If you want to " + \
+            "overwrite, you must pass the -f, --force flag.")
 
     return args
 
@@ -74,10 +107,12 @@ def parse_address_args(args):
             continue
 
     if invalid_addrs:
-        print(f"Found {len(invalid_addrs)} invalid IP addresses on input: {invalid_addrs}")
+        print(f"Found {len(invalid_addrs)} invalid IP addresses on " +\
+            "input: {invalid_addrs}")
 
     if valid_addrs:
-        print(f"Found {len(valid_addrs)} valid IP addresses on input: {valid_addrs}")
+        print(f"Found {len(valid_addrs)} valid IP addresses on " + \
+            "input: {valid_addrs}")
     else:
         print(f"No valid addresses found on input")
 
