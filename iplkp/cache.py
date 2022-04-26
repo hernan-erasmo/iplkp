7c5b1f01a9f2014e0eb95e3b9bbedad9a005baaf
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 19:33:22 -0300

    lint(cache): fix linter camel-case warnings

--- iplkp/cache.py
+++ iplkp/cache.py
@@ -24,8 +24,9 @@ class IplkpCache():
                 required_keys = [RDAP_LOOKUP_TASK_NAME, GEO_IP_LOOKUP_TASK_NAME]
                 if not all([key in data for key in required_keys]):
                     raise ValueError("Invalid cache file")
-        except Exception as e:
-            print(f"Unable to read from cache data source ({e}). Starting with empty cache.")
+        except Exception as exception:
+            print(f"Unable to read from cache data source ({exception})." + \
+                "Starting with empty cache.")
         else:
             return data
 
@@ -33,16 +34,16 @@ class IplkpCache():
             with open(CACHE_DATASOURCE, "w") as data_source:
                 file_structure = self._init_empty()
                 json.dump(file_structure, data_source)
-        except Exception as e:
-            raise IplkpCacheException(f"Unable to create cache file ({e})")
+        except Exception as exception:
+            raise IplkpCacheException(f"Unable to create cache file ({exception})")
         else:
             return file_structure
 
-    def get_rdap(self, ip):
-        return ip in self._data_source[RDAP_LOOKUP_TASK_NAME]
+    def get_rdap(self, ip_address):
+        return ip_address in self._data_source[RDAP_LOOKUP_TASK_NAME]
 
-    def get_geo_ip(self, ip):
-        return ip in self._data_source[GEO_IP_LOOKUP_TASK_NAME]
+    def get_geo_ip(self, ip_address):
+        return ip_address in self._data_source[GEO_IP_LOOKUP_TASK_NAME]
 
     def update(self, results):
         for key in results.keys():
@@ -69,7 +70,7 @@ class IplkpCache():
               about both for the given ip
         """
         cached_results = self._init_empty()
-        missing_IPs = []
+        missing_ips = []
 
         query_all = just_rdap == just_geo
 
@@ -82,17 +83,17 @@ class IplkpCache():
                     rdap_cached_results[ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
                     geo_ip_cached_results[ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
                 else:
-                    missing_IPs.append(ip)
+                    missing_ips.append(ip)
             elif just_rdap:
                 if self.get_rdap(ip):
                     rdap_cached_results[ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
                 else:
-                    missing_IPs.append(ip)
+                    missing_ips.append(ip)
             elif just_geo:
                 if self.get_geo_ip(ip):
                     geo_ip_cached_results[ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
                 else:
-                    missing_IPs.append(ip)
+                    missing_ips.append(ip)
 
         # Remove the keys the user didn't ask for
         if not query_all and just_geo:
@@ -102,5 +103,5 @@ class IplkpCache():
 
         print("returning cached results for " + \
             f"{min([len(cached_results[k]) for k in cached_results.keys()])} IPs" + \
-            f", missing {len(missing_IPs)}")
-        return cached_results, missing_IPs
+            f", missing {len(missing_ips)}")
+        return cached_results, missing_ips
