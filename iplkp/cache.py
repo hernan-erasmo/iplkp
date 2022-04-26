be4ea89258139151b6a11004138a6feeb5fc4a3a
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 19:29:31 -0300

    lint(cache): fix line length warnings

--- iplkp/cache.py
+++ iplkp/cache.py
@@ -56,7 +56,7 @@ class IplkpCache():
             - true or false if it's an rdap query
             - true or false if it's a geo_ip query
             (both true means it's both an rdap and geo_ip query)
-            
+
             Returns:
             - two lists:
                 - the first one with addresses found inside the cache
@@ -65,36 +65,42 @@ class IplkpCache():
             an address is considered to be in the cache if
             - it's an rdap_query and the cache contains rdap info about the address
             - it's a geo_ip query and the cache contains geo_ip info about the address
-            - it's both an rdap and geo_ip query, and the cache contains info about both for the given ip
+            - it's both an rdap and geo_ip query, and the cache contains info
+              about both for the given ip
         """
         cached_results = self._init_empty()
         missing_IPs = []
 
         query_all = just_rdap == just_geo
 
+        rdap_cached_results = cached_results[RDAP_LOOKUP_TASK_NAME]
+        geo_ip_cached_results = cached_results[GEO_IP_LOOKUP_TASK_NAME]
+
         for ip in addresses:
             if query_all:
                 if all([self.get_rdap(ip), self.get_geo_ip(ip)]):
-                    cached_results[RDAP_LOOKUP_TASK_NAME][ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
-                    cached_results[GEO_IP_LOOKUP_TASK_NAME][ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
+                    rdap_cached_results[ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
+                    geo_ip_cached_results[ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
                 else:
                     missing_IPs.append(ip)
             elif just_rdap:
                 if self.get_rdap(ip):
-                    cached_results[RDAP_LOOKUP_TASK_NAME][ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
+                    rdap_cached_results[ip] = self._data_source[RDAP_LOOKUP_TASK_NAME][ip]
                 else:
                     missing_IPs.append(ip)
             elif just_geo:
                 if self.get_geo_ip(ip):
-                    cached_results[GEO_IP_LOOKUP_TASK_NAME][ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
+                    geo_ip_cached_results[ip] = self._data_source[GEO_IP_LOOKUP_TASK_NAME][ip]
                 else:
                     missing_IPs.append(ip)
-        
+
         # Remove the keys the user didn't ask for
         if not query_all and just_geo:
             del cached_results[RDAP_LOOKUP_TASK_NAME]
         if not query_all and just_rdap:
             del cached_results[GEO_IP_LOOKUP_TASK_NAME]
 
-        print(f"returning cached results for {min([len(cached_results[k]) for k in cached_results.keys()])} IPs, missing {len(missing_IPs)}")
+        print("returning cached results for " + \
+            f"{min([len(cached_results[k]) for k in cached_results.keys()])} IPs" + \
+            f", missing {len(missing_IPs)}")
         return cached_results, missing_IPs
