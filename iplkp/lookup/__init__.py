5c5f69e0d68216d8eed8968c18dead164767fe33
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 21:10:27 -0300

    lint(lookup): fix lint warnings

--- iplkp/lookup/__init__.py
+++ iplkp/lookup/__init__.py
@@ -1,3 +1,6 @@
+"""
+The lookup module contains the logic related to IP lookup operations
+"""
 import asyncio
 import time
 from iplkp.lookup.geo_ip import geo_ip_lookup
@@ -7,8 +10,40 @@ from iplkp.cache import IplkpCache, IplkpCacheException
 
 
 async def run_queries(ip_list, just_rdap=False, just_geo=False):
+    """Create asyncronous queries for RDAP and Geo IP lookups.
+
+    Each query is wrapped into an asyncronous task and executed within an
+    asyncio event loop, saving a lot of time in I/O wait.
+
+    Args:
+      ip_list:
+        A list of IP addresses.
+      just_rdap:
+        True if only RDAP information should be queried. (default=False)
+      just_geo:
+        True if only Geo IP information should be queried. (default=False)
+
+    Returns:
+      A dict mapping an rdap key to dicts containing information about IPs and their
+      rdap data, and a geo ip key mapping to dicts containing information about IPs
+      and their geo ip data. Example:
+
+      {"rdap_lookup":
+        {"192.168.1.1":
+          {"asn": ...}
+        , "8.8.8.8": {
+          {"asn": ...}
+        },
+       "ip_lookup":
+        {"192.168.1.1":
+          {"country": ...}
+        , "8.8.8.8": {
+          {"country": ...}
+        }
+      }
+    """
     tasks = []
-   
+
     query_all = not any([just_rdap, just_geo])
 
     if just_rdap or query_all:
@@ -22,11 +57,45 @@ async def run_queries(ip_list, just_rdap=False, just_geo=False):
     if tasks:
         await asyncio.gather(*tasks)
         return {task.get_name():task.result() for task in tasks}
-    else:
-        return {}
+    return {}
 
 
 def lookup(ip_list, just_rdap=False, just_geo=False, use_cache=True):
+    """Queries RDAP and/or Geo IP information about addresses.
+
+    It also checks whether the information about any given IP is inside
+    the application cache and can return that information, avoiding sending
+    a request to an external server.
+
+    Args:
+      ip_list:
+        A list of IP addresses.
+      just_rdap:
+        True if only RDAP information should be queried. (default=False)
+      just_geo:
+        True if only Geo IP information should be queried. (default=False)
+      use_cache:
+        True if results should be fetched from cache if possible. (default=True)
+
+    Returns:
+      A dict mapping an rdap key to dicts containing information about IPs and their
+      rdap data, and a geo ip key mapping to dicts containing information about IPs
+      and their geo ip data. Example:
+
+      {"rdap_lookup":
+        {"192.168.1.1":
+          {"asn": ...}
+        , "8.8.8.8": {
+          {"asn": ...}
+        },
+       "ip_lookup":
+        {"192.168.1.1":
+          {"country": ...}
+        , "8.8.8.8": {
+          {"country": ...}
+        }
+      }
+    """
     results = {}
     cache = None
 
@@ -34,13 +103,15 @@ def lookup(ip_list, just_rdap=False, just_geo=False, use_cache=True):
         try:
             cache = IplkpCache()
         except IplkpCacheException as ex:
-            print(f"{ex}. This is not an issue with iplkp. If the problem persists, try calling iplkp with --no-cache")
+            print(f"{ex}. This is not an issue with iplkp. If the problem persists, " + \
+                "try calling iplkp with --no-cache")
         else:
-            cached_data, missing_IPs = cache.find_all(ip_list, just_rdap=just_rdap, just_geo=just_geo)
-            if not missing_IPs:
+            cached_data, missing_ips = cache.find_all(ip_list,
+                                                        just_rdap=just_rdap,
+                                                        just_geo=just_geo)
+            if not missing_ips:
                 return cached_data
-            else:
-                ip_list = missing_IPs
+            ip_list = missing_ips
 
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     time_start = time.time()
