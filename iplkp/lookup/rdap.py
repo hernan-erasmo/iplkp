74b91c3f3a283a9ee7e03b614adf4670286faf33
Hernán Erasmo <dherasmo@gmail.com>
2022-04-26 22:22:08 -0300

    lint(rdap): fix lint warnings

--- iplkp/lookup/rdap.py
+++ iplkp/lookup/rdap.py
@@ -1,8 +1,12 @@
 import asyncio
-import aiohttp
 from itertools import cycle
+import aiohttp
 from iplkp.utils import show_remaining_tasks
-from iplkp.consts import RDAP_CONNECTIONS_LIMIT, RDAP_CONNECTIONS_LIMIT_PER_HOST, RDAP_LOOKUP_TASK_PREFIX, IPLKP_EXCEPTION_KEY
+from iplkp.consts import (
+    RDAP_CONNECTIONS_LIMIT,
+    RDAP_CONNECTIONS_LIMIT_PER_HOST,
+    RDAP_LOOKUP_TASK_PREFIX,
+    IPLKP_EXCEPTION_KEY)
 
 RDAP_LOOKUP_ERROR_KEY = "iplkp_rdap_error"
 
@@ -14,39 +18,45 @@ RDAP_URLS = {
     'afrinic': 'http://rdap.afrinic.net/rdap/ip/'
 }
 
-def parse_rdap_lookup_content(ip, status, content):
+def parse_rdap_lookup_content(ip_address, status, content):
     results = {}
     if status != 200:
-        results[ip] = {
+        results[ip_address] = {
             RDAP_LOOKUP_ERROR_KEY: status,
             "message": "Error fetching RDAP info for the given IP"
         }
     else:
-        results[ip] = content
+        results[ip_address] = content
     return results
 
-async def fetch_rdap(url, ip, session):
-    query_url = f"{url}{ip}"
+async def fetch_rdap(url, ip_address, session):
+    query_url = f"{url}{ip_address}"
     try:
         async with session.get(query_url) as response:
             show_remaining_tasks()
             content = await response.json()
-            return parse_rdap_lookup_content(ip, response.status, content)
-    except Exception as e:
-        return {ip: {IPLKP_EXCEPTION_KEY: f"Exception while calling fetch_rdap: {repr(e)}"}}
+            return parse_rdap_lookup_content(ip_address, response.status, content)
+    except Exception as exception:
+        return {
+            ip_address: {
+                IPLKP_EXCEPTION_KEY: f"Exception while calling fetch_rdap: {repr(exception)}"
+            }
+        }
 
 async def rdap_lookup(ip_list):
     tasks = []
     rdap_urls = cycle(RDAP_URLS.values())
     timeout = aiohttp.ClientTimeout(total=None, sock_connect=10, sock_read=60)
-    connector = aiohttp.TCPConnector(limit=RDAP_CONNECTIONS_LIMIT, limit_per_host=RDAP_CONNECTIONS_LIMIT_PER_HOST)
+    connector = aiohttp.TCPConnector(limit=RDAP_CONNECTIONS_LIMIT,
+                                    limit_per_host=RDAP_CONNECTIONS_LIMIT_PER_HOST)
     async with aiohttp.ClientSession(connector=connector, timeout=timeout) as rdap_session:
-        for task_number, ip in enumerate(ip_list):
+        for task_number, ip_address in enumerate(ip_list):
             url = next(rdap_urls)
-            task = asyncio.create_task(fetch_rdap(url, ip, rdap_session), name=f"{RDAP_LOOKUP_TASK_PREFIX}{task_number}")
+            task = asyncio.create_task(
+                fetch_rdap(url, ip_address, rdap_session), name=f"{RDAP_LOOKUP_TASK_PREFIX}{task_number}")
             tasks.append(task)
         task_results = await asyncio.gather(*tasks)
     responses = {}
-    for t in task_results:
-        responses |= t
+    for result in task_results:
+        responses |= result
     return responses
